d794e6244bb85ed005326d4225fc445b5eb3579c
Mayank4545 <keshri.mayank@gmail.com>
2016-11-13 13:24:15 +0530

    ASCII Format
    Converting the characters for ASCII Format.

---
+++ ASCIIFormat.java
@@ -0,0 +1,15 @@
+public class ASCIIFormat {
+public static void main(String[] args) {
+	
+//	char chr = 'A';
+//	int ascii = chr;
+//	System.out.println(ascii);
+	
+	int num = 65;
+	for(num = 65; num <= 90; num++){
+		char ascii = (char) num;
+		System.out.println(num + " - " + ascii);
+			
+	}	
+}
+}
